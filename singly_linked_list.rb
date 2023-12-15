10fdb6c08cae0751471ed57ea5371ae5090256e0
Ashish Garg <01ashishgarg@gmail.com>
2023-12-15 20:43:57 +0530

    DS: Added Single Linked List

---
+++ singly_linked_list.rb
@@ -0,0 +1,91 @@
+# Head -> Node -> Tail
+# Operations -> Insert/Remove/Traverse
+
+class Node
+  attr_accessor :value, :next
+
+  def initialize(value)
+    self.value = value
+    self.next = nil
+  end
+end
+
+class SinglyLinkedList
+  attr_accessor :head, :tail, :length
+
+  def initialize
+    self.head = nil
+    self.tail = nil
+    self.length = 0
+  end
+
+  def insert(item)
+    node = Node.new(item)
+    head.nil? ? self.head = node : self.tail.next = node
+    self.tail = node
+    self.length += 1
+  end
+
+  # The complexity of this method is O(n).
+  def remove(node)
+    return nil if node.nil?
+
+    if node == head
+      if self.head.next.nil?
+        self.head = self.tail = nil
+      else
+        self.head = self.head.next
+      end
+    else
+      tmp = self.head
+      while tmp && tmp.next != node
+        tmp = tmp.next
+      end
+      tmp.next = node.next if tmp
+    end
+    self.length -= 1
+  end
+
+  # The complexity of this method is O(1).
+  def cat(list)
+    return nil if list.nil?
+
+    self.tail.next = list.head
+    self.length += list.length
+  end
+
+  # This method removes all elements from the list. Since we have to walk the whole list, the complexity is O(n).
+  def clear
+    while self.length > 0
+      remove self.head
+    end
+  end
+
+  def find_first(&predicate)
+    return nil unless block_given?
+
+    current = self.head
+    while current
+      return current if predicate.call(current)
+      current = current.next
+    end
+  end
+
+  def each
+    return nil unless block_given?
+
+    current = self.head
+    while current
+      yield current
+      current = current.next
+    end
+  end
+
+  def print
+    if self.length == 0
+      puts "empty"
+    else
+      self.each { |item| puts item.value }
+    end
+  end
+end
